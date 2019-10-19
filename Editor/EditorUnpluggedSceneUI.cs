0ed8dbfdf0f48432f3f6a12c69b8997aa2503930
Cratesmith <cratesmith@cratesmith.com>
2019-10-19 17:06:16 +1000

    initial commit

---
+++ Editor/EditorUnpluggedSceneUI.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace EditorUnplugged
+{
+    [InitializeOnLoad]
+    public class EditorUnpluggedSceneUI 
+    {
+        static GUIStyle s_LabelStyle;
+        static GUIStyle s_ToggleStyle;
+        static GUIStyle s_TextFieldStyle;
+        static GUIStyle s_TimeLabelStyle;
+
+        static double s_LastRepaint = -1;
+        static bool s_MousePanning;
+
+        static Texture2D s_TimeIcon;
+        static Texture2D s_GreenLightIcon;
+        static Texture2D s_YellowLightIcon;
+        static Texture2D s_RedLightIcon;
+        static Texture2D s_ScriptIcon;
+        static Texture2D s_CompileIcon;
+        static Texture2D s_TextureIcon;
+        static Texture2D s_LightingIcon;
+
+        static EditorUnpluggedSceneUI()
+        {
+#if UNITY_2019_1_OR_NEWER
+            SceneView.duringSceneGui += OnSceneGUI;
+#else
+            SceneView.onSceneGUIDelegate += OnSceneGUI;
+#endif
+            CompilationPipeline.assemblyCompilationStarted += OnCompileStart;
+            EditorApplication.update += OnUpdate;
+        }
+
+        static void OnUpdate()
+        {
+            if (EditorApplication.timeSinceStartup - s_LastRepaint > 60)
+            {
+                SceneView.RepaintAll();
+            }
+        }
+
+        static void OnCompileStart(string _obj)
+        {
+            SceneView.RepaintAll();
+        }
+
+        static void OnSceneGUI(SceneView _sceneview)
+        {
+            // don't repaint if we're panning
+            if (Tools.current == Tool.View || Tools.current== Tool.Custom)
+            {
+                return;
+            }
+            
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+            {
+                s_MousePanning = true;
+            }
+
+            if (s_MousePanning)
+            {
+                if (Event.current.type == EventType.MouseLeaveWindow || Event.current.type==EventType.MouseUp && Event.current.button == 1)
+                {
+                    EditorApplication.delayCall += () => s_MousePanning = false; // just to do this outside of gui events
+                }
+                return;
+            }
+
+            if (s_LabelStyle==null)
+            {
+                s_LabelStyle = new GUIStyle("Label");
+            }
+
+            if (s_ToggleStyle == null)
+            {
+                s_ToggleStyle = new GUIStyle("Toggle");
+            }
+            
+            if (s_TextFieldStyle == null)
+            {
+                s_TextFieldStyle = new GUIStyle("MiniTextField");
+            }
+
+            if (s_TimeLabelStyle == null)
+            {
+                s_TimeLabelStyle = new GUIStyle("helpbox");
+                s_TimeLabelStyle.alignment = TextAnchor.MiddleCenter;
+                s_TimeLabelStyle.padding.top = 1;
+                s_TimeLabelStyle.padding.bottom = 1;
+            }
+
+            if (s_TimeIcon == null)
+            {
+                s_TimeIcon = EditorGUIUtility.FindTexture("TestStopwatch");
+            }
+
+            if (s_GreenLightIcon == null)
+            {
+                s_GreenLightIcon = EditorGUIUtility.FindTexture("winbtn_mac_max");
+            }
+            
+            if (s_YellowLightIcon == null)
+            {
+                s_YellowLightIcon = EditorGUIUtility.FindTexture("winbtn_mac_min");
+            }
+            
+            if (s_RedLightIcon == null)
+            {
+                s_RedLightIcon = EditorGUIUtility.FindTexture("winbtn_mac_close");
+            }
+
+            if (s_ScriptIcon == null)
+            {
+                s_ScriptIcon = EditorGUIUtility.FindTexture("cs Script Icon");
+            }
+
+            if (s_CompileIcon == null)
+            {
+                s_CompileIcon = EditorGUIUtility.FindTexture("WaitSpin00");
+            }
+
+            if (s_TextureIcon == null)
+            {
+                s_TextureIcon = EditorGUIUtility.FindTexture("animationdopesheetkeyframe");
+            }
+            
+            if (s_LightingIcon == null)
+            {
+                s_LightingIcon = EditorGUIUtility.FindTexture("Main Light Gizmo");
+            }
+            
+            Handles.BeginGUI();
+            GUILayout.BeginArea(new Rect(5, 5, Screen.width, 22));
+            using (new GUILayout.HorizontalScope( GUILayout.ExpandWidth(false), GUILayout.Height(20)))
+            {
+                using (new GUILayout.HorizontalScope("HelpBox", GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                {
+                    {
+                        var r = GUILayoutUtility.GetRect(12,12);
+                        GUI.Label(new Rect(r.xMin-4, r.yMin-1, r.height+8, r.height+8), new GUIContent(s_TimeIcon, "Session duration"));
+                    }
+                    var timespan = TimeSpan.FromSeconds(EditorApplication.timeSinceStartup);
+                    GUILayout.Label($"{timespan.Hours.ToString("00")}h {timespan.Minutes.ToString("00")}m", s_LabelStyle, GUILayout.ExpandWidth(false));
+                }
+
+                if (SystemInfo.batteryStatus != BatteryStatus.Unknown)
+                {
+                    using (new GUILayout.HorizontalScope("HelpBox", GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                    {
+                        {
+                            var r = GUILayoutUtility.GetRect(12,12); r.yMin = r.yMin + 4;
+                            if (SystemInfo.batteryLevel > .6f)
+                            {
+                                GUI.DrawTexture(r, s_GreenLightIcon, ScaleMode.ScaleToFit, true, 1);
+                            }else if (SystemInfo.batteryLevel > .3f)
+                            {
+                                GUI.DrawTexture(r, s_YellowLightIcon,ScaleMode.ScaleToFit, true, 1);
+                            }
+                            else
+                            {
+                                GUI.DrawTexture(r, s_RedLightIcon,ScaleMode.ScaleToFit, true, 1);
+                            }
+                        }
+                        var current = SystemInfo.batteryLevel * 100f;
+                        var used = EditorUnpluggedUtil.BatteryUsed * 100f;
+                        GUILayout.Label(SystemInfo.batteryStatus.ToString(), s_LabelStyle, GUILayout.ExpandWidth(false));
+                        GUILayout.Label($"{current.ToString("F1")}%", s_LabelStyle, GUILayout.ExpandWidth(false));
+                        GUILayout.Label(new GUIContent($"{(used < 0 ? "+" : "-")}{Mathf.Abs(used).ToString("F1")}%","Percent used this session"), s_TimeLabelStyle, GUILayout.Width(55));
+
+                        var timeRemainingA = EditorUnpluggedUtil.BatteryTimeRemainingShortSample;
+                        var timeRemainingB = EditorUnpluggedUtil.BatteryTimeRemainingLongSample;
+                        var minRemaining = timeRemainingA < timeRemainingB ? timeRemainingA : timeRemainingB;
+                        var maxRemaining = timeRemainingA >= timeRemainingB ? timeRemainingA : timeRemainingB;
+                        
+                        if (!float.IsInfinity(minRemaining))
+                        {
+                            var timespan = TimeSpan.FromSeconds(minRemaining);
+                            if(timespan.Days < 1) GUILayout.Label(new GUIContent($"{timespan.Hours.ToString("00")}h {timespan.Minutes.ToString("00")}m", "Estimated Remaining (Min)"), s_TimeLabelStyle, GUILayout.Width(60));
+
+                            if (!float.IsInfinity(maxRemaining) && !Mathf.Approximately(maxRemaining, minRemaining))
+                            {
+                                GUILayout.Label("->");
+                                var maxtimespan = TimeSpan.FromSeconds(maxRemaining);
+                                if (maxtimespan.Days < 1)
+                                {
+                                    GUILayout.Label(new GUIContent($"{maxtimespan.Hours.ToString("00")}h {maxtimespan.Minutes.ToString("00")}m","Estimated Remaining (Max)"), s_TimeLabelStyle, GUILayout.Width(60));
+                                }
+                            }
+                            GUILayout.Label("@");
+
+                            var rate1h = EditorUnpluggedUtil.BatteryRateShortSample*100f*60f*60f;
+                            if(rate1h < 1000f) GUILayout.Label(new GUIContent($"{rate1h.ToString("F1")}/h", "Recent usage percent per hour"), s_TimeLabelStyle, GUILayout.Width(55));
+                        }
+                    }
+                }
+
+                using (new GUILayout.HorizontalScope("HelpBox",GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                {
+                    EditorUnpluggedUtil.FrameLimiter = GUILayout.Toggle(EditorUnpluggedUtil.FrameLimiter, "Fps limit", s_ToggleStyle, GUILayout.ExpandWidth(false));
+                    if (int.TryParse(EditorGUILayout.DelayedTextField(EditorUnpluggedUtil.FrameLimiterFPS.ToString(), s_TextFieldStyle, GUILayout.Width(20)),
+                        out var newTargetFps))
+                    {
+                        EditorUnpluggedUtil.FrameLimiterFPS = newTargetFps;
+                    }                    
+                }
+                
+                using (new GUILayout.HorizontalScope("HelpBox",GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                {
+                    {
+                        var r = GUILayoutUtility.GetRect(12,12);
+                        GUI.Label(new Rect(r.xMin-4, r.yMin-1, r.height+8, r.height+8), new GUIContent(s_ScriptIcon, "Asset Reload/Script Compile"));
+                    }
+                    GUILayout.Label(EditorUnpluggedUtil.CompileCount.ToString(), s_LabelStyle, GUILayout.ExpandWidth(false));
+                    EditorUnpluggedUtil.AutoRefresh = GUILayout.Toggle(EditorUnpluggedUtil.AutoRefresh, "Auto", s_ToggleStyle, GUILayout.ExpandWidth(false));
+                    if (!EditorUnpluggedUtil.AutoRefresh)
+                    {
+                        if (GUILayout.Button(new GUIContent("Ctrl-R", "Reimport assets/compile"), "minibutton"))
+                        {
+                            AssetDatabase.Refresh();
+                        }
+                    }
+                    
+                    if (EditorApplication.isCompiling)
+                    {     
+                        var r = GUILayoutUtility.GetRect(12,12);
+                        GUI.Label(new Rect(r.xMin-4, r.yMin-1, r.height+8, r.height+8), new GUIContent(s_CompileIcon, "Compiling! (how did you see this tooltip!?)"));
+                    }
+
+                    if (EditorUnpluggedUtil.LastCompileDuration > 0 && EditorUnpluggedUtil.CompileCount > 0)
+                    { 
+                        GUILayout.Label(new GUIContent($"{EditorUnpluggedUtil.LastCompileDuration.ToString("00.0")}s", "Last Script Reload Duration"), s_TimeLabelStyle, GUILayout.Width(40));
+                    }
+                }
+
+                using (new GUILayout.HorizontalScope("HelpBox", GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                {
+                    var r = GUILayoutUtility.GetRect(12,12);
+                    GUI.Label(new Rect(r.xMin-4, r.yMin-1, r.height+8, r.height+8), new GUIContent(s_TextureIcon, "When to compress assets"));
+                    EditorUnpluggedUtil.CompressOnImport = EditorGUILayout.Popup(EditorUnpluggedUtil.CompressOnImport ? 0 : 1,
+                                                         new[] {"On Build", "On Import"}, GUILayout.Width(70))==1;
+                }
+                
+                using (new GUILayout.HorizontalScope("HelpBox", GUILayout.ExpandWidth(false), GUILayout.Width(1)))
+                {
+                    var r = GUILayoutUtility.GetRect(12,12);
+                    GUI.Label(new Rect(r.xMin-4, r.yMin-1, r.height+8, r.height+8), new GUIContent(s_LightingIcon, "Lightmapping workflow"));
+                    Lightmapping.giWorkflowMode = (Lightmapping.GIWorkflowMode)EditorGUILayout.EnumPopup(Lightmapping.giWorkflowMode,GUILayout.Width(75));
+                    if (Lightmapping.giWorkflowMode == Lightmapping.GIWorkflowMode.OnDemand)
+                    {
+                        if (GUILayout.Button(new GUIContent("Bake", "Bake lightmaps"), "minibutton"))
+                        {
+                            Lightmapping.BakeAsync();
+                        }
+                    }
+                }
+            }
+            GUILayout.EndArea();
+            Handles.EndGUI();
+            
+            s_LastRepaint = EditorApplication.timeSinceStartup;
+        }
+    }
+}
---
+++ Editor/EditorUnpluggedUtil.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace EditorUnplugged
+{
+    public static class EditorUnpluggedUtil
+    {
+        public static double LastCompileDuration => LastCompileTimeEnd - LastCompileTimeStart;
+        
+        const string EditorPrefs_CrunchOnImport = "kCompressTexturesOnImport";
+        
+        public static bool CompressOnImport
+        {
+            get => EditorPrefs.GetBool(EditorPrefs_CrunchOnImport, false);
+            set => EditorPrefs.SetBool(EditorPrefs_CrunchOnImport, value);
+        }
+        
+        const string EditorPrefs_AutoRefresh = "kAutoRefresh";
+
+        public static bool AutoRefresh
+        {
+            get => EditorPrefs.GetBool(EditorPrefs_AutoRefresh, false);
+            set => EditorPrefs.SetBool(EditorPrefs_AutoRefresh, value);
+        }
+        
+        const string EditorPrefs_CompileCount = "EditorUnpluggedUtil.CompileCount";
+        public static int CompileCount
+        {
+            get => EditorPrefs.GetInt(EditorPrefs_CompileCount, 0);
+            private set => EditorPrefs.SetInt(EditorPrefs_CompileCount, value);
+        }
+        
+        const string EditorPrefs_LastReloadTime = "EditorUnpluggedUtil.LastReloadTime";
+        public static double LastReloadTime
+        {
+            get => EditorPrefs_GetDouble(EditorPrefs_LastReloadTime, 0);
+            private set => EditorPrefs_SetDouble(EditorPrefs_LastReloadTime, value);
+        }
+
+        static double EditorPrefs_GetDouble(string _editorPrefsLastReloadTime, int _default)
+        {
+            return double.TryParse(EditorPrefs.GetString(_editorPrefsLastReloadTime), out var result)
+                ? result
+                : _default;
+        }
+
+        static void EditorPrefs_SetDouble(string _editorPrefsLastReloadTime, double _value)
+        {
+            EditorPrefs.SetString(_editorPrefsLastReloadTime, _value.ToString());
+        }
+
+        const string EDITORPREFS_LastCompileTimeStart = "ScriptExecutionOrder.LastCompileTimeStart";
+        public static double LastCompileTimeStart
+        {
+            get => EditorPrefs_GetDouble(EDITORPREFS_LastCompileTimeStart, 0);
+            private set { EditorPrefs_SetDouble(EDITORPREFS_LastCompileTimeStart, value);}
+        }
+        
+        const string EDITORPREFS_LastCompileTimeEnd = "ScriptExecutionOrder.LastCompileTimeEnd";
+        public static double LastCompileTimeEnd
+        {
+            get => EditorPrefs_GetDouble(EDITORPREFS_LastCompileTimeEnd, 0);
+            private set { EditorPrefs_SetDouble(EDITORPREFS_LastCompileTimeEnd, value);}
+        }
+        
+        const string EditorPrefs_BatterySample1Time = "EditorUnpluggedUtil.BatterySample1Time";
+        static double BatterySample1Time
+        {
+            get => EditorPrefs_GetDouble(EditorPrefs_BatterySample1Time, 0);
+            set => EditorPrefs_SetDouble(EditorPrefs_BatterySample1Time, value);
+        }
+        
+        const string EditorPrefs_BatterySample2Time = "EditorUnpluggedUtil.BatterySample2Time";
+        static double BatterySample2Time
+        {
+            get => EditorPrefs_GetDouble(EditorPrefs_BatterySample2Time, 0);
+            set => EditorPrefs_SetDouble(EditorPrefs_BatterySample2Time, value);
+        }
+        
+        const string EditorPrefs_BatterySample1 = "EditorUnpluggedUtil.BatterySample1";
+        static float BatterySample1
+        {
+            get => EditorPrefs.GetFloat(EditorPrefs_BatterySample1, 0);
+            set => EditorPrefs.SetFloat(EditorPrefs_BatterySample1, value);
+        }
+
+        const string EditorPrefs_BatterySample2 = "EditorUnpluggedUtil.BatterySample2";
+        static float BatterySample2
+        {
+            get => EditorPrefs.GetFloat(EditorPrefs_BatterySample2, 0);
+            set => EditorPrefs.SetFloat(EditorPrefs_BatterySample2, value);
+        }
+        
+        const string EditorPrefs_BatteryRateLongSample = "EditorUnpluggedUtil.BatteryRateLongSample";
+        public static double BatteryRateLongSample
+        {
+            get => EditorPrefs_GetDouble(EditorPrefs_BatteryRateLongSample, 0);
+            private set => EditorPrefs_SetDouble(EditorPrefs_BatteryRateLongSample, value);
+        }
+        
+        const string EditorPrefs_StartBattery = "EditorUnpluggedUtil.StartBattery";
+        public static float StartBattery
+        {
+            get => EditorPrefs.GetFloat(EditorPrefs_StartBattery, 1);
+            private set => EditorPrefs.SetFloat(EditorPrefs_StartBattery, value);
+        }
+
+        const string EditorPrefs_WasCompiling = "EditorUnpluggedUtil.WasCompiling";
+        public static bool WasCompiling
+        {
+            get => EditorPrefs.GetBool(EditorPrefs_WasCompiling, false);
+            private set => EditorPrefs.SetBool(EditorPrefs_WasCompiling, value);
+        }
+
+        public static float CurrentBattery => SystemInfo.batteryLevel >= 0f ? SystemInfo.batteryLevel : 1f;
+        public static float BatteryUsed => StartBattery - SystemInfo.batteryLevel;
+        
+
+        public static bool FrameLimiter
+        {
+            get => QualitySettings.vSyncCount == 0;
+            set => QualitySettings.vSyncCount = value?0:1;
+        }
+
+        public static int FrameLimiterFPS
+        {
+            get => Application.targetFrameRate;
+            set => Application.targetFrameRate = value;
+        }
+        
+        public static double BatteryRateShortSample => (CurrentBattery - BatterySample2) / (EditorApplication.timeSinceStartup - BatterySample2Time);
+
+        public static float BatteryTimeRemainingShortSample => BatteryRateShortSample < -0.00001 ? Mathf.Max(0,(float)(-SystemInfo.batteryLevel/BatteryRateShortSample)):float.PositiveInfinity;
+        
+        public static float BatteryTimeRemainingLongSample => BatteryRateLongSample < -0.00001 ? Mathf.Max(0,(float)(-SystemInfo.batteryLevel/BatteryRateLongSample)):float.PositiveInfinity;
+
+        [InitializeOnLoadMethod()]
+        static void InitializeOnLoad()
+        {
+            if (LastReloadTime > 0 && EditorApplication.timeSinceStartup < LastReloadTime)
+            { 
+                OnEditorLaunch();
+            }
+            LastReloadTime = EditorApplication.timeSinceStartup;
+            
+            EditorApplication.update += OnUpdate;
+        }
+
+        [DidReloadScripts(9999)]
+        static void OnReloadScripts()
+        {
+            if (WasCompiling)
+            {
+                WasCompiling = false;
+                ++CompileCount;
+                LastCompileTimeEnd =  EditorApplication.timeSinceStartup;
+//                Debug.Log($"Complication ended at {EditorApplication.timeSinceStartup} duration={LastCompileDuration}");
+            }
+            CompilationPipeline.assemblyCompilationStarted += OnCompileStarted;
+        }
+
+        const float BatterySampleDelay = 30f;
+        
+        static void OnUpdate()
+        {
+            if (EditorApplication.timeSinceStartup - BatterySample1Time >= BatterySampleDelay && !Mathf.Approximately(BatterySample1,CurrentBattery))
+            {
+                BatterySample2Time = BatterySample1Time;
+                BatterySample2 = BatterySample1;
+
+                BatterySample1Time = EditorApplication.timeSinceStartup;
+                BatterySample1 = CurrentBattery;
+
+                BatteryRateLongSample = BatteryRateShortSample*0.9 + BatteryRateLongSample*0.1;
+            }
+        }
+
+        static void OnCompileStarted(string _)
+        {
+            if (!WasCompiling)
+            {
+                LastCompileTimeStart = EditorApplication.timeSinceStartup;
+//                Debug.Log($"Complication started at {EditorApplication.timeSinceStartup}");
+                WasCompiling = true;
+                
+                ClosePackageManagerWindows();
+            }
+        }
+
+        static void ClosePackageManagerWindows()
+        {
+            // close the package manager to save battery and 10s of compile time
+            var pmwt = GetType("UnityEditor.PackageManager.UI.PackageManagerWindow");
+            var pmw = (EditorWindow[]) Resources.FindObjectsOfTypeAll(pmwt);
+            if (pmw.Length <= 0)
+            {
+                return;
+            }
+            
+            Debug.Log("Closing package manager windows to avoid excessive compile time");
+            foreach (var window in pmw)
+            {
+                window.Close();
+            }
+        }
+
+        static Type GetType(string name)
+        { 
+            Type type = null;
+            var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                type = assemblies[i].GetType(name);
+                if (type != null) break;
+            }
+            return type;
+        }
+        
+        static void OnEditorLaunch()
+        {
+            Debug.Log("Editor Launch Detected");
+            CompileCount = 0;
+            BatterySample1 = BatterySample2 = StartBattery = SystemInfo.batteryLevel;
+            LastCompileTimeStart = LastCompileTimeStart = 0;
+            BatterySample1Time = 0;
+            FrameLimiterFPS = 30;
+            Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+            Lightmapping.ForceStop();
+        }
+    }
+}
